3549a2b85261d603f369ff76381bceb7c927d37f
Peter Mooney <peter.mooney90@gmail.com>
2015-06-18 08:28:38 -0400

    Add video record functionality

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
+// RecordRTC stream objects
+var videoRecord;
+var audioRecord;
+
+// References to playback elements
+var audio;
+var video;
+
 // When all the content on the page loaded.
 window.addEventListener("DOMContentLoaded", function() {
+
+  // Assign HTML elements to references.
+  audio = document.getElementById("audio-playback");
+  video = document.getElementById("video-playback");
+
   // Assign getUserMedia to vendor specific version
   navigator.getUserMedia = navigator.getUserMedia ||
                            navigator.webkitGetUserMedia ||
@@ -8,19 +21,51 @@ window.addEventListener("DOMContentLoaded", function() {
 
   // If the browser supports video/audio functionality
   if (navigator.getUserMedia) {
+
     // Ask for audio/video permission
     navigator.getUserMedia({video: true, audio: true},
-    // Permission granted, stream webcam to video element.
+
+    // Permission granted, stream webcam to video element
+    // and start recording.
     function(stream) {
-      video.src= window.URL.createObjectURL(stream); 
+
+      // Set video reference to camera stream
+      video.src= window.URL.createObjectURL(stream);
+
+      // Create audio stream and begin recording
+      audioRecord = new RecordRTC(stream, {type: 'audio'});
+      audioRecord.startRecording();
+
+      // Create video stream and begin recording.
+      videoRecord = new RecordRTC(stream, {type: 'video'});
+      videoRecord.startRecording();
     },
+
     // Permission denied, print error to log
     function(err) {
       console.log("The following error occured: " + err.name);
     });
+
   // Browser does not support audio/video, print to log.
   } else {
     console.log("getUserMedia not supported");
   }
 
 });
+
+// When the "STOP" button is pressed
+function stopRecording() {
+
+  // Stop the audio recording and play reuslt in audio element.
+  audioRecord.stopRecording(function (audioURL) {
+    audio.src = audioURL;
+    audio.play;
+  });
+
+  // Stop the video recording and play reuslt in video element.
+  videoRecord.stopRecording(function (videoURL) {
+    video.src = videoURL;
+    video.play;
+  });
+
+};
